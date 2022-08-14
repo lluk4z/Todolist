b9855d70593e91222a92e54acbc35c84dd525cfd
lluk4z <lemmg@cin.ufpe.br>
2022-08-14 13:44:09 -0300

    implementação da classe tarefa e suas restrições

--- app.py
+++ app.py
@@ -23,6 +23,76 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    content = db.Column(db.Text)
+    done = db.Column(db.Boolean, default=False)
+    priority = db.Column(db.Text, default='Baixa')
+
+    def __init__(self, name, content, priority):
+        self.name = name
+        self.content = content
+        self.done = False
+        self.priority = priority
+
+    def __repr__(self):
+        return '<Name %s> <Content %s>' % self.name % self.content
+
+
+db.create_all()
+
+@app.route('/list')
+def tasks_list():
+    tasks = Task.query.all()
+    return render_template('list.html', tasks=tasks)
+
+@app.route('/task', methods=['POST'])
+def add_task():
+    content = request.form['content']
+
+    name = request.form['name']
+
+    priority = request.form['priority']
+
+    if not name:
+        return 'Error'
+
+
+    if not content:
+        return 'Error'
+
+
+    task = Task(name,content, priority)
+    db.session.add(task)
+    db.session.commit()
+    return redirect('list')
+
+
+@app.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if not task:
+        return redirect('list')
+
+    db.session.delete(task)
+    db.session.commit()
+    return redirect('list')
+
+
+@app.route('/done/<int:task_id>')
+def resolve_task(task_id):
+    task = Task.query.get(task_id)
+
+    if not task:
+        return redirect('list')
+    if task.done:
+        task.done = False
+    else:
+        task.done = True
+
+    db.session.commit()
+    return redirect('list')
 
 if __name__ == '__main__':
     app.run(debug=True)
